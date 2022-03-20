b5bf07a852e82084891ec7329b5348f88573fa81
Maciej Wiatr <maciekwiatr17@gmail.com>
2022-03-20 13:43:04 +0100

    Remove unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,87 +1,11 @@
 import "./App.css";
-import {
-	Box,
-	Button,
-	Divider,
-	Flex,
-	FormControl,
-	FormHelperText,
-	FormLabel,
-	Grid,
-	GridItem,
-	HStack,
-	NumberDecrementStepper,
-	NumberIncrementStepper,
-	NumberInput,
-	NumberInputField,
-	NumberInputStepper,
-	Spinner,
-	Switch,
-	Text,
-} from "@chakra-ui/react";
-import { useRef, useState } from "react";
-import { useForm } from "react-hook-form";
-import {
-	Elevator as ElevatorType,
-	ElevatorSystem,
-	IElevatorSystem,
-} from "./core/ElevatorSystem";
-import { useInterval } from "react-use";
+import { Grid, GridItem } from "@chakra-ui/react";
 import { ElevatorVis } from "./components/ElevatorVis";
 import { IElevatorFormData } from "./types";
 import ElevatorForm from "./components/ElevatorForm";
 import { useElevatorSystem } from "./hooks";
 
-interface ISystemRef {
-	elevatorSystem: IElevatorSystem;
-}
-
 const App = () => {
-	// const [floorNo] = useState(14);
-	// let systemRef = useRef<ISystemRef>({
-	// 	elevatorSystem: new ElevatorSystem(1),
-	// });
-	// const [elevatorStatus, setElevatorStatus] = useState<ElevatorType[]>([]);
-	// const [runSimulation, setRunSimulation] = useState(false);
-
-	// const addPassenger = (floorNumber: number) => {
-	// 	console.log("add passenger", floorNumber);
-	// 	systemRef.current.elevatorSystem.pickup(
-	// 		floorNumber,
-	// 		Math.random() > 0.5 ? 2 : -2
-	// 	);
-	// };
-
-	// const onSubmit = (data: IElevatorFormData) => {
-	// 	if (runSimulation) {
-	// 		setRunSimulation(false);
-	// 		return;
-	// 	}
-
-	// 	const elevatorSystem = new ElevatorSystem(data.elevatorNumber);
-
-	// 	if (data.passengerNumber) {
-	// 		for (let i = 0; i < data.passengerNumber; i++) {
-	// 			const randomFloor = Math.floor(Math.random() * floorNo) - 8;
-	// 			let randomDest = Math.random() > 0.5 ? 2 : -2;
-	// 			elevatorSystem.pickup(randomFloor, randomDest);
-	// 		}
-	// 	}
-
-	// 	systemRef.current.elevatorSystem = elevatorSystem;
-	// 	setRunSimulation(true);
-	// };
-
-	// useInterval(
-	// 	() => {
-	// 		systemRef.current.elevatorSystem.step();
-	// 		setElevatorStatus((s) => [
-	// 			...systemRef.current.elevatorSystem.status(),
-	// 		]);
-	// 	},
-	// 	runSimulation ? 300 : null
-	// );
-
 	const {
 		elevatorStatus,
 		addPassenger,
